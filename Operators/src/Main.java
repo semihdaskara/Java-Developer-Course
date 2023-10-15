6c6d021487a079ffb286541f55434f2e9c0e4851
Semih Daşkara <semihdaskara35@gmail.com>
2023-10-15 22:16:05 +0300

    Operators
    Operators

---
+++ Operators/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println(3 + 4.2);
+		System.out.println(3 - 5f);
+		System.out.println(10 / 4.0);
+		System.out.println(3 * 4.2);
+		System.out.println(3 % 4);
+
+		int a = 4;
+		a = a + 2;
+		System.out.println(a);
+		
+		a++; //posfix
+		++a; // prefix
+		
+		System.out.println(a);
+
+	}
+}

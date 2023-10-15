9ad663dc866a0c0d7ce32c1527e57af6981403c8
Semih Daşkara <semihdaskara35@gmail.com>
2023-10-15 22:14:35 +0300

    FindingHypotenuse-Project
    FindingHypotenuse-Project

---
+++ FindingHypotenuse/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int a, b;
+		System.out.print("Birinci Kenar: ");
+		a = scanner.nextInt();
+		System.out.print("İkinci Kenar: ");
+		b = scanner.nextInt();
+
+		double h = Math.sqrt(a * a + b * b);
+		System.out.print("Hipotenüs: " + h);
+
+	}
+
+}
